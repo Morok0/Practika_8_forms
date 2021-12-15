1c663e40e180b2f45d8fd84f16785d6e08a3e61c
Morok0 <95218742+Morok0@users.noreply.github.com>
2021-12-15 10:39:38 +0300

    Add files via upload

---
+++ Pr_8_zad_1_forms/Pr_8_zad_1_forms/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pr_8_zad_1_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String line = " ";
+            line = Convert.ToString(textBox1.Text);
+            
+            int length = int.Parse(textBox2.Text);
+            textBox3.Text=($"{string.Join(" ", Regex.Matches(line, @"\w+").Cast<Match>().Where(t => t.Length == length))}");
+        }
+    }
+}
